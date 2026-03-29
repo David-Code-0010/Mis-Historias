109a29ae94e149d94e1a09eb10d1d9d9c2e21dd0
David-Code-0010 <david.r.programmer@gmail.com>
2026-03-29 00:44:44 -0700

    Delete src directory

--- src/app/story/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreateStory() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Escribir una nueva historia</h1>
-      <p className="text-gray-500">Aquí irá tu formulario pronto...</p>
-    </div>
-  );
-}
